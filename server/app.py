2cd0a33d9ea556238a6fdd855d4bfdc2b40b6658
deepu718 <deeputi718@gmail.com>
2023-09-06 01:06:15 +0530

    Add existing file

---
+++ server/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import util
+
+st.title("Banglore House Price Prediction :house:")
+
+util.load_saved_artifacts()
+
+loc,area = st.columns(2)
+
+location = loc.selectbox("Location",util.get_location_names())
+
+area_sq_ft = area.number_input("numbers",min_value=700,max_value=15000,value=1000,step=5)
+
+bhk,bath,balcony = st.columns(3)
+
+bhk = bhk.slider("bhk",min_value=1,max_value=5,value=2,step=1)
+
+bath = bath.slider("bath",min_value=1,max_value=5,value=2,step=1)
+
+balcony = balcony.slider("balcony",min_value=1,max_value=5,value=1,step=1)
+
+if st.button("Predict"):
+    price = util.get_estimated_price(location,area_sq_ft,bhk,bath,balcony)
+    st.write(f"Predicted Price of the house is {price} lakhs")
